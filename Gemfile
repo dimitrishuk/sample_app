b3faa4e7e14e16b950eccaca0ee45b207fd0d428
Dimitri Shukuroglou <dimitrishuk@gmail.com>
2012-08-09 23:49:47 +0100

    rspec

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ end
 group :development, :test do
   gem 'sqlite3'
   gem 'rspec-rails', "~> 2.0"
+  gem "capybara"
 end
 # Gems used only for assets and not required
 # in production environments by default.
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,9 +4,9 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the content 'Sample'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_content('Sample')
     end
   end
 end
